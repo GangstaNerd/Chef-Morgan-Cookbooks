c10b0fedc7736e7772324e868eb16cdbb52ff00d
Morgan Willis <gangstanizzerd@gmail.com>
2016-12-07 21:29:25 -0500

    Added Program
    Java Program added

---
+++ Hello.java
@@ -0,0 +1,24 @@
+#
+#
+#Basic Java Program to add to git
+#
+#
+# Copyright © 2000–2011, Robert Sedgewick and Kevin Wayne.
+#Last updated: Tue Aug 30 09:58:33 EDT 2016.
+
+public class TenHelloWorlds {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+
+}
